8d41cb79b0707e2e1d75ce8b8378a141493e0905
Lawton Nichols <lawtonnichols@gmail.com>
2020-04-03 18:11:57 -0700

    .

--- lec03/functions.cpp
+++ lec03/functions.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 using namespace std;
 
+// declare functions and implement them later
+int absolute_value(int x);
+int sum(int x, int y);
+void hello_world();
+
+int main(int argc, char *argv[])
+{
+    cout << "absolute value of -2 is: " << absolute_value(-2) << endl;
+
+    cout << "sum of 2 and 3 is: " << sum(2, 3) << endl;
+
+    hello_world();
+
+    return 0;
+}
+
 // absolute value function
 // takes an int, returns an int
 int absolute_value(int x) {
@@ -20,15 +36,4 @@ int sum(int x, int y) {
 // it doesn't return anything, so its return "type" is "void"
 void hello_world() {
     cout << "Hello world!\n";
-}
-
-int main(int argc, char *argv[])
-{
-    cout << "absolute value of -2 is: " << absolute_value(-2) << endl;
-
-    cout << "sum of 2 and 3 is: " << sum(2, 3) << endl;
-
-    hello_world();
-
-    return 0;
 }
